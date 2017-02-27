551f8644974fd90713a8c1a2f499a3e923b99729
PaulEdouard Boudier <paul-edouard.boudier@laposte.net>
2017-02-27 16:47:19 +0100

    Added model user with all attributes but profil picture

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170227152425) do
+ActiveRecord::Schema.define(version: 20170227154242) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -28,6 +28,14 @@ ActiveRecord::Schema.define(version: 20170227152425) do
     t.inet     "last_sign_in_ip"
     t.datetime "created_at",                          null: false
     t.datetime "updated_at",                          null: false
+    t.string   "first_name"
+    t.string   "last_name"
+    t.string   "address"
+    t.boolean  "gender"
+    t.string   "nationality"
+    t.date     "date_of_birth"
+    t.string   "long_description"
+    t.string   "short_description"
     t.index ["email"], name: "index_users_on_email", unique: true, using: :btree
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
   end
