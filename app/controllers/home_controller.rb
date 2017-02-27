6ae0f8747c4e761b91b92110b548920128217fd1
PaulEdouard Boudier <paul-edouard.boudier@laposte.net>
2017-02-27 17:29:56 +0100

    modified routes, now good to go, and appplication controller

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,4 @@
-class HomeController < ApplicationController
+class HomesController < ApplicationController
   skip_before_action :authenticate_user!, only: [ :home ]
 
   def welcome
--- app/controllers/menus_controller.rb
+++
@@ -1,6 +0,0 @@
-class MenusController < ApplicationController
-  skip_before_action :authenticate_user!, only: [ :home ]
-
-  def home
-  end
-end
