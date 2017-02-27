5649888310409dbb4c7705aace7d6906c1dd7da9
PaulEdouard Boudier <paul-edouard.boudier@laposte.net>
2017-02-27 18:04:55 +0100

    controlelrs problem, now solved by working on masterb branch

---
+++ app/controllers/teds_controller.rb
@@ -0,0 +1,23 @@
+class TedsController < ApplicationController
+
+  def index
+  end
+
+  def show
+  end
+
+  def new
+  end
+
+  def create
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  def delete
+  end
+end
