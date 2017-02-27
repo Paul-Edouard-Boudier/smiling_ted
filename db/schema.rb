08da006ea1084e73f771621092cb1ea62572a6cd
PaulEdouard Boudier <paul-edouard.boudier@laposte.net>
2017-02-27 16:52:50 +0100

    added model Ted, rank, distance and photo aren't included yet

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170227155026) do
+ActiveRecord::Schema.define(version: 20170227155156) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -31,6 +31,15 @@ ActiveRecord::Schema.define(version: 20170227155026) do
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
     t.index ["chapter_id"], name: "index_pages_on_chapter_id", using: :btree
+  end
+
+  create_table "teds", force: :cascade do |t|
+    t.string   "name"
+    t.integer  "unique_id"
+    t.integer  "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_teds_on_user_id", using: :btree
   end
 
   create_table "users", force: :cascade do |t|
@@ -60,4 +69,5 @@ ActiveRecord::Schema.define(version: 20170227155026) do
 
   add_foreign_key "chapters", "users"
   add_foreign_key "pages", "chapters"
+  add_foreign_key "teds", "users"
 end
