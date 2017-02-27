916531ac7dabe856655f70e0fbcd0386b4569625
PaulEdouard Boudier <paul-edouard.boudier@laposte.net>
2017-02-27 16:50:07 +0100

    added Chapter modules with all attributes but teddybear references

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170227154242) do
+ActiveRecord::Schema.define(version: 20170227154842) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "chapters", force: :cascade do |t|
+    t.integer  "user_id"
+    t.string   "description"
+    t.string   "localisation"
+    t.integer  "likes"
+    t.datetime "created_at",   null: false
+    t.datetime "updated_at",   null: false
+    t.index ["user_id"], name: "index_chapters_on_user_id", using: :btree
+  end
 
   create_table "users", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
@@ -40,4 +50,5 @@ ActiveRecord::Schema.define(version: 20170227154242) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
   end
 
+  add_foreign_key "chapters", "users"
 end
