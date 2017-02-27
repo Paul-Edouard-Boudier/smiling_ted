646fbf503743e29639b29f13d23ddbef8d021e02
Matthieu Cartillier <m.carttos@gmail.com>
2017-02-27 18:43:26 +0100

    added new nested routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,24 +1,11 @@
 Rails.application.routes.draw do
 
-  get 'pages/index'
-
-  get 'pages/show'
-
-  get 'pages/new'
-
-  get 'pages/create'
-
-  get 'pages/edit'
-
-  get 'pages/update'
-
-  get 'pages/delete'
-
   devise_for :users
   root to: 'homes#welcome'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  resources :teds
-  resources :chapters
-
-
+  resources :teds do
+    resources :chapters do
+      resources :pages
+    end
+  end
 end
