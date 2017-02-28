0dfe9f23e80864140088f3f756338b584c991fe7
Matthieu Cartillier <m.carttos@gmail.com>
2017-02-28 16:03:49 +0100

    changement of model photo

--- db/seeds.rb
+++ db/seeds.rb
@@ -10,3 +10,12 @@ user = User.create(
   Ted.create(name: Faker::Name.first_name, user_id: user.id)
 
 end
+
+urls = [
+  'https://www.hillingdonstp.org/assets/_data/_council/_events/l/hcp1444039799.jpg',
+  'http://teddybearpages.com/travel/amsterdam/amsterdam-canal.jpg'
+]
+
+photo = Photo.new
+photo.save!
+photo.photo_urls = urls
