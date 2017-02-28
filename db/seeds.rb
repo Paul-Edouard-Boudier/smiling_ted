31e27a62f13e18e84c1093067d1b21dfb3a166b6
Matthieu Cartillier <m.carttos@gmail.com>
2017-02-28 19:03:52 +0100

    commit before deconnection

--- db/seeds.rb
+++ db/seeds.rb
@@ -3,14 +3,32 @@ require 'faker'
 Ted.destroy_all
 User.destroy_all
 
+
 user = User.create(
   email:"polo@hello.com" ,
   password:"password" ,
   password_confirmation: "password"
 )
 
+
 5.times do
-  ted = Ted.create(name: Faker::Name.first_name, user_id: user.id)
+
+  autre = User.create(
+    email: Faker::Internet.email,
+    password: "password",
+    password_confirmation: "password",
+    first_name: Faker::Name.first_name,
+    last_name: Faker::Name.last_name,
+    avatar_url: 'http://lorempixel.com/50/50'
+  )
+
+  ted = Ted.create(
+    name: Faker::Name.first_name,
+    user_id: user.id,
+    rank: Faker::Number.between(1, 5),
+    distance_traveled: Faker::Number.between(10, 3000),
+    code: Faker::Lorem.characters(6)
+  )
 
   chapter = ted.chapters.create(
     user: user,
@@ -27,3 +45,6 @@ user = User.create(
     )
   end
 end
+
+
+
