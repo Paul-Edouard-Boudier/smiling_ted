b82c5134f466e1f5403d19524de909bc5ef3c81c
Matthieu Cartillier <m.carttos@gmail.com>
2017-03-06 14:40:55 +0100

    added redirection to current location

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,6 +3,8 @@ class ApplicationController < ActionController::Base
   before_action :authenticate_user!
   before_action :configure_permitted_parameters, if: :devise_controller?
   before_action :set_locale
+  before_filter :store_current_location, unless: :devise_controller?
+
 
   def set_locale
     I18n.locale = params[:locale] || I18n.default_locale
@@ -16,4 +18,9 @@ class ApplicationController < ActionController::Base
     # For additional fields in app/views/devise/registrations/new.html.erb
     devise_parameter_sanitizer.permit(:sign_up, keys: [:first_name, :last_name])
   end
+
+  def store_current_location
+    store_location_for(:user, request.url)
+  end
+
 end
