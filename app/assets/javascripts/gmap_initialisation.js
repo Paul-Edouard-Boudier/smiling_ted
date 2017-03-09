8d7a620ad0924554b273eeb24be3fc1ff0f72998
Matthieu Cartillier <m.carttos@gmail.com>
2017-03-09 17:50:39 +0100

    improve map

--- app/assets/javascripts/gmap_initialisation.js
+++ app/assets/javascripts/gmap_initialisation.js
@@ -5,7 +5,12 @@ $(document).ready(function() {
     var lng = $(map).data('longitude');
 
     var handler = Gmaps.build('Google');
-    handler.buildMap({ internal: { id: id } }, function() {
+    handler.buildMap({ internal: { id: id },
+      provider: {
+      styles: [{"featureType":"landscape.natural","elementType":"geometry.fill","stylers":[{"visibility":"on"},{"color":"#e0efef"}]},{"featureType":"poi","elementType":"geometry.fill","stylers":[{"visibility":"on"},{"hue":"#1900ff"},{"color":"#c0e8e8"}]},{"featureType":"road","elementType":"geometry","stylers":[{"lightness":100},{"visibility":"simplified"}]},{"featureType":"road","elementType":"labels","stylers":[{"visibility":"off"}]},{"featureType":"transit.line","elementType":"geometry","stylers":[{"visibility":"on"},{"lightness":700}]},{"featureType":"water","elementType":"all","stylers":[{"color":"#7dcdcd"}]}]
+      }
+
+      }, function() {
 
       markers = handler.addMarker({
         lat: lat,
