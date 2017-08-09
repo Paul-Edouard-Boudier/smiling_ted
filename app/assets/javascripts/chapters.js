bc7ee42bf071febb2ab6333fb096b15fe0b4450e
Matthieu Cartillier <m.carttos@gmail.com>
2017-08-09 16:34:16 +0200

    fix chapter.js to be able to push in heroku some

--- app/assets/javascripts/chapters.js
+++ app/assets/javascripts/chapters.js
@@ -1,14 +1,14 @@
 $(document).ready(function() {
 
-  $.attachinary.config.template = `
-    <div>
-      <% for(var i=0; i<files.length; i++){ %>
-        <img
-          src="<%= $.cloudinary.url(files[i].public_id, { "version": files[i].version, "format": 'jpg', "crop": 'fill', "width": 150, "height": 200 }) %>"
-          alt="" width="150" height="200" />
-      <% } %>
-    </div>
-  `
+    $.attachinary.config.template = '\
+      <div>\
+          <% for(var i=0; i<files.length; i++){ %>\
+            <img\
+              src="<%= $.cloudinary.url(files[i].public_id, { "version": files[i].version, "format": "jpg", "crop": "fill", "width": 150, "height": 200 }) %>"\
+              alt="" width="150" height="200" />\
+          <% } %>\
+        </div>\
+    ';
 
   $(document)
    .on('drop dragover', function (e) {
