83f1875765586d43cc4b680eaa4ee92a4ac1430b
Matthieu Cartillier <m.carttos@gmail.com>
2017-08-09 23:30:37 +0200

    fix pundit to allow only  admin to acces administration

--- app/controllers/admin/application_controller.rb
+++ app/controllers/admin/application_controller.rb
@@ -6,7 +6,28 @@
 # you're free to overwrite the RESTful controller actions.
 module Admin
   class ApplicationController < Administrate::ApplicationController
-    before_action :authenticate_user!
-    include Pundit
+    before_action :authenticate_admin!
+
+    def authenticate_admin!
+      if current_user
+        user_not_authorized unless current_user.admin
+      else
+        getlogin
+      end
+    end
+
+
+    private
+
+    def user_not_authorized
+      flash[:alert] = "It seems that you are not an admin"
+      redirect_to root_path
+    end
+
+    def getlogin
+      flash[:notice] = "Thanks to login with admin credantials"
+      redirect_to new_user_session_path
+    end
+
   end
 end
